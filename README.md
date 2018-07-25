a06d3321758248ce23e31d1dd41d278ef7b449b1
Tiago Martinelli <tiagomartinelli@MacBook-Pro-de-Tiago.local>
2018-07-25 09:22:26 -0300

    Base structure
    Main tasks, fonts, sass components and libs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# portfolio
-personal website
+
+# Simple personal website Gulp - Twig template
+#
+#
+* * *
+### Setup:
+1. __Clone the project:__ `git clone https://github.com/tiagomtnl/portfolio.git`
+2. __Install Dependencies:__ `npm install`
+3. __Configure Gulp.js file:__
+    * Remember to adjust your path for source and public path
+5. __Run default task:__ `gulp`
+#
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+// *********************************** VARIABLES *********************************** //
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+
+var sass = require('gulp-sass');
+var twig = require('gulp-twig');
+const babel = require('gulp-babel');
+
+// *********************************** PATHS *********************************** //
+
+// Source paths
+var input = './app'; 
+var inputSass = input + '/sass/**/*.scss'; // Sass to Css
+var inputTwig = input + '/template/*.twig'; // Twig to HTML
+var inputJs = input + '/js/**/*.js'; // JS
+// Dist paths
+var output = './public'; 
+var outputSass = output + '/css/';  // Sass to Css
+var outputTwig = output;  // Twig to HTML
+var outputJs = output + '/js/'; // ES6 to JS
+
+// *********************************** TASKS *********************************** //
+
+// Compile twig template
+gulp.task('twigCompile', function () {
+    return gulp.src(input + '/template/index.twig')
+        .pipe(twig({
+            data: {} // include Data
+        }))
+        .pipe(gulp.dest(outputTwig));
+});
+
+// Compile Scss
+gulp.task('sass', function() {
+    return gulp.src(inputSass)
+        .pipe(sass())
+        .pipe(gulp.dest(outputSass))
+        .pipe(browserSync.stream());
+});
+
+// Compile JS
+gulp.task ('scripts', function(){
+    gulp.src(inputJs)
+    .pipe(babel({
+        presets: ['env']
+    }))
+    .pipe(gulp.dest(outputJs))
+    .pipe(browserSync.stream());
+});
+
+// Fonts to public
+gulp.task('fonts', function() {
+    return gulp.src(input + '/fonts/*')
+        .pipe(gulp.dest(output + '/fonts/'));
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'twigCompile'], function() {
+
+    // Base directory of application
+    browserSync.init({
+        server: output
+    });
+
+    // Watch files
+    gulp.watch(inputSass, ['sass']);
+    gulp.watch(inputTwig, ['twigCompile']);
+    gulp.watch(inputJs, ['scripts']);
+    gulp.watch("./public/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
